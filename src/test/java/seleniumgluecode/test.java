168e5a83ae9c69ff91d4903600b1ebcde6da975d
HeccasDeccas <Declan.E.Schillert@student.uts.edu.au>
2018-09-24 13:54:58 +1000

    Partially Implemented Cucumber

---
+++ src/test/java/seleniumgluecode/test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seleniumgluecode;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+/**
+ *
+ * @author decla
+ */
+public class test {
+    
+    
+}
