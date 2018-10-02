7581b933de95fb009531d2ea7f7c8443d3e1f0a3
Ash <ashleigh.l.wan@student.uts.edu.au>
2018-10-02 22:11:01 +1000

    Changed author and some test stuff

--- src/test/java/features/test.feature
+++ src/test/java/features/test.feature
@@ -7,3 +7,9 @@ Scenario: User opens Login page
     Given user opens browser
     When login page is loaded
     Then login button is active
+
+Scenario: UTS Staff member completes form
+    Given UTS Staff Member logs in
+    When completing order form
+    Then system should have both drop down options and text fields
+
--- src/test/java/seleniumgluecode/test.java
+++ src/test/java/seleniumgluecode/test.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package seleniumgluecode;
+
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -19,10 +20,12 @@ import org.junit.Assert;
  * @author decla
  */
 public class test {
+
     WebDriver driver;
+
     @Given("^user opens browser$")
-    public void user_opens_browser() throws Throwable {     
-    	//System.setProperty("webdriver.chrome.driver","/Users/Documents/chromedriver");
+    public void user_opens_browser() throws Throwable {
+        //System.setProperty("webdriver.chrome.driver","/Users/Documents/chromedriver");
         Class<? extends WebDriver> driverClass = ChromeDriver.class;
         WebDriverManager.getInstance(driverClass).setup();
         driver = driverClass.newInstance();
@@ -31,22 +34,23 @@ public class test {
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //driver.get("http://automationpractice.com/index.php");
     }
-        
+
     @When("^login page is loaded$")
     public void login_page_is_loaded() throws Throwable {
-    	//driver.findElement(By.id("ID")).sendKeys("123456");
+        //driver.findElement(By.id("ID")).sendKeys("123456");
         //driver.findElement(By.id("password")).sendKeys("blahblah123");
         //driver.findElement(By.id("SubmitLogin")).click(); 
         driver.navigate().to(driver.getCurrentUrl());
     }
-    
+
     @Then("^login button is active$")
     public void login_button_is_active() throws Throwable {
-    	if(driver.findElement(By.id("Login")).isEnabled()) {
+        if (driver.findElement(By.id("Login")).isEnabled()) {
             System.out.println("Login button enabled");
         } else {
             System.out.println("Login button disabled");
         }
-        driver.close(); 
+        driver.close();
     }
+
 }
